680f9547c9cbcf27cd998ed9942a791ffd3308b2
GrimmOutlook <grimmoutlook87@gmail.com>
2018-01-27 09:49:06 -0500

    Wasted time trying to figure out where to put the Redirect conditional in the TripInvite component.  And I'm sure the TravisCI build will fail for no good reason as well.

--- src/components/trip-invite.js
+++ src/components/trip-invite.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-// import {Redirect} from 'react-router-dom';
+import {Route, Redirect, withRouter} from 'react-router-dom';
 // import Dashboard from './components/dashboard';
 // import LoginPage from './components/login-page';
 import {storeInviteUUID} from '../actions/index';
@@ -8,26 +8,55 @@ import {storeInviteUUID} from '../actions/index';
 
 export class TripInvite extends React.Component {
 
+  // redirectToDashboard(){
+  //   return (<Redirect to="/dashboard" />);
+  // }
+
+  // redirectToLogin(){
+  //   return (<Redirect to="/login" />);
+  // }
+
   changeUUIDState(e, inviteUUID){
     e.preventDefault();
     console.log('TripInvite inviteUUID before dispatch: ', inviteUUID);
     this.props.dispatch(storeInviteUUID(inviteUUID));
+
+    // return (
+    //     <Route exact path="/" render={() => (
+    //       this.props.loggedIn ? (
+    //         <Redirect to="/dashboard"/>
+    //       ) : (
+    //         <Redirect to="/login"/>
+    //       )
+    //     )}/>
+    // )
+
+    // { this.props.loggedIn ? (<Redirect to="/dashboard" />) : (<Redirect to="/login" />) }
+
+    // { this.props.loggedIn ? this.redirectToDashboard() : this.redirectToLogin() }
+
   }
 
   render() {
     const inviteUUID = this.props.match.params.tripUUID;
-    console.log(`inviteUUID: ${inviteUUID}`);
-    console.log(`this.props.inviteUUIDInStore: ${this.props.inviteUUIDInStore}`);
 
     return (
 
       <div>
+        {/* <Route exact path="/" render={() => (
+          this.props.loggedIn ? (
+            <Redirect to="/dashboard"/>
+          ) : (
+            <Redirect to="/login"/>
+          )
+        )}/> */}
+
         <h1>Welcome!  You have received an invite for trip code {inviteUUID}</h1>
         <h2>Click button below to add to your dashboard:</h2>
         <button onClick={(e) => this.changeUUIDState(e, inviteUUID)}>Dummy button for updating inviteUUID state</button>
         <h3>inviteUUIDInStore: {this.props.inviteUUIDInStore}</h3>
 
-        {/* { authToken ? <Redirect exact path="/dashboard" component={Dashboard} /> : <Redirect exact path="/login" component={LoginPage} /> } */}
+        {/* { this.props.loggedIn ? this.redirectToDashboard() : this.redirectToLogin() } */}
       </div>
 
     )
@@ -36,7 +65,9 @@ export class TripInvite extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  inviteUUIDInStore: state.inviteUUID.inviteUUID
+  inviteUUIDInStore: state.inviteUUID.inviteUUID,
+  loggedIn: state.auth.currentUser !== null
 })
 
+// export default withRouter(connect(mapStateToProps)(TripInvite));
 export default connect(mapStateToProps)(TripInvite);
